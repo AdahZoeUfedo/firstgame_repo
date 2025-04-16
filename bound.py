cb0427940d82699abf96a92e7294d56e2e0156f0
adah_zoe_ufedoojo <zoeadah25@gmail.com>
2025-04-16 13:43:47 +0100

    committing my images

--- bound.py
+++ bound.py
@@ -1,10 +1,18 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Skybound")
+clock = pygame.time.Clock()
+
+sky_surface = pygame.image.load('firstgame_repo/Sky.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              pygame.quit()
+             exit()
+             
     pygame.display.update() 
+    clock.tick(60)
