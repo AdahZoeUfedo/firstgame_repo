83fc61e415be773aee9118ef8a8924626d060d9f
adah_zoe_ufedoojo <zoeadah25@gmail.com>
2025-04-16 17:36:05 +0100

    Sky, ground and font added

--- bound.py
+++ bound.py
@@ -5,14 +5,20 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Skybound")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('firstgame_repo/font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('firstgame_repo/Sky.png')
+sky_surface=pygame.image.load('firstgame_repo/Sky.png')
+ground_surface=pygame.image.load('firstgame_repo/ground.png')
+text_surface = test_font.render('My game', False, 'black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              pygame.quit()
              exit()
-             
+    screen.blit(sky_surface,(0,0))  
+    screen.blit(ground_surface,(0,300)) 
+    screen.blit(text_surface,(300,50)) 
+           
     pygame.display.update() 
     clock.tick(60)
